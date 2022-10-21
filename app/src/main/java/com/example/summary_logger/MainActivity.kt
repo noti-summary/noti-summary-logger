dc8abd8960eee80ff9fa1c8e34f4a906d45b006b
james5418 <james900425@gmail.com>
2022-10-22 00:32:04 +0800

    [feat] change userId AlertDialog into jetpack compose

--- app/src/main/java/com/example/summary_logger/MainActivity.kt
+++ app/src/main/java/com/example/summary_logger/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.summary_logger
 
-import android.app.AlertDialog
-import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
-import android.text.InputType
-import android.util.Log
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.summary_logger.database.UserDao
-import com.example.summary_logger.database.UserDatabase
-import com.example.summary_logger.model.User
 import com.example.summary_logger.ui.theme.SummaryloggerTheme
-import kotlinx.coroutines.*
+import com.example.summary_logger.jetpack_compose.UserIdAlertDialog
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,37 +19,15 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
+                    UserIdAlertDialog(this)
                 }
             }
         }
 
-        setUserId(this)
-
     }
 
 }
 
-fun setUserId(context: Context){
-    val sharedPref = context.getSharedPreferences("user_id", Context.MODE_PRIVATE)
-
-    if(sharedPref.getString("user_id", "000").toString() == "000"){
-        val builder = AlertDialog.Builder(context)
-        builder.setMessage("Please enter your user id")
-
-        var input = EditText(context)
-        input.inputType = InputType.TYPE_CLASS_TEXT
-        builder.setView(input)
-
-        builder.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
-            with (sharedPref.edit()) {
-                putString("user_id", input.text.toString())
-                apply()
-            }
-            Toast.makeText(context, "user_id = ${sharedPref.getString("user_id", "000").toString()}", Toast.LENGTH_LONG).show()
-        })
-        builder.show()
-    }
-}
 
 @Composable
 fun Greeting(name: String) {
